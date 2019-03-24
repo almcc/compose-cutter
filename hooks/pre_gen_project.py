21807c1ebaac1dcca1e9424ed2c59de7cb35d321
Alastair McClelland <alastair.mcclelland@gmail.com>
2019-03-24 13:28:23 +0000

    Added in images

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 import os
+import sys
 
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+PROJECT_DIR = os.path.realpath(os.path.curdir)
+PARENT_DIR = os.path.dirname(PROJECT_DIR)
+
+if os.path.exists(os.path.join(PARENT_DIR, 'docker-compose.yaml')):
+    print('Error: docker-compose.yaml already exists.')
+    sys.exit(1)
